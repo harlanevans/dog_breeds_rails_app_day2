bf982fdc538927b35d43380f34fa55529fd56e2a
Harlan Evans <harlanevans5@gmail.com>
2018-11-13 16:28:21 -0700

    added show and index

---
+++ app/controllers/dogs_controller.rb
@@ -0,0 +1,19 @@
+class DogsController < ApplicationController
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+  end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
