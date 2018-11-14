559e71009e9f2f398fd223cdbc3802797dde97ce
Harlan Evans <harlanevans5@gmail.com>
2018-11-13 17:04:46 -0700

    added some css

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -11,9 +11,39 @@ class DogsController < ApplicationController
     @dog = Dog.new
   end
 
+  def create
+    @dog = Dog.new(dogs_params)
+  
+    if @dog.save
+      redirect_to dogs_path
+    else
+      render :new
+    end
+  end
+
   def edit
+    @dog = Dog.find(params[:id])
+  end
+
+  def update
+    @dog = Dog.find(params[:id])
+
+    if @dog.update(dogs_params)
+      redirect_to dogs_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @dog = Dog.find(params[:id]).destroy
+    redirect_to dogs_path
   end
+  
+
+  private
+
+    def dogs_params
+      params.require(:dog).permit(:name, :breed, :age)
+    end
 end
